fba6bbae82ce5c9d1f722450e9f1aa2d61df19aa
jsundram <jsundram@gmail.com>
2013-04-04 12:51:52 -0700

    make the install work if library path is not set in preferences.txt

--- install-lx.sh
+++ install-lx.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 SKETCHBOOK=`awk '/sketchbook.path/ {print substr($1, 17);}' ~/Library/Processing/preferences.txt`
+if [ ${#SKETCHBOOK} -lt 2 ]
+then
+    SKETCHBOOK=~/Documents/Processing
+fi
+
 mkdir -p $SKETCHBOOK/libraries
 rm -fr $SKETCHBOOK/libraries/HeronLX
 unzip -q external/HeronLX.zip -d $SKETCHBOOK/libraries
